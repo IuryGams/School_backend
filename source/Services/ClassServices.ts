4f18b775309be985d64bf2926d7bfd3106c391fa
Iury Gama <Iuryempregos@gmail.com>
2025-02-26 17:50:03 -0300

    create CRUD for ClassServices

---
+++ source/Services/ClassServices.ts
@@ -0,0 +1,64 @@
+import { Services } from "."
+
+class ClassServices extends Services<"class"> {
+    
+    
+    public async createClass(name: string, subjectId: number, teacherId: number) {
+        const newClass = await this.create({
+            data: {
+                name,
+                teacherId,
+                subjectId
+            }
+        });
+
+        return newClass;
+    }
+
+    public async getClassByID(classId: number) {
+        const foundClass = await this.findUnique({
+            where: {id: classId},
+            include: {
+                subject: true,
+                teacher: true,
+                enrollments: true
+            }
+        });
+
+        return foundClass;
+    }
+
+
+    public async getAllClasses() {
+        const classes = await this.findMany({
+            include: {
+                subject: true,
+                teacher: true,
+                enrollments: true
+            }
+        });
+
+        return classes;
+    }
+
+    public async updateClassInfor(classId: number, name?: string, subjectId?: number, teacherId?: number) {
+        const updatedClass = await this.update({
+            where: {id: classId},
+            data: {
+                name,
+                subjectId,
+                teacherId
+            }
+        })
+
+        return updatedClass;
+    }
+
+    public async deleteClass(classId: number):Promise<void> {
+        await this.delete({
+            where: {id: classId}
+        });
+    }
+}
+
+export {ClassServices }
