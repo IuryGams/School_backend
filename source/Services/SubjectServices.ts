02cbd02f4f9d56b61794261cbed4bbd331f3ac1a
Iury Gama <Iuryempregos@gmail.com>
2025-02-26 18:48:36 -0300

    create SubjectServices

---
+++ source/Services/SubjectServices.ts
@@ -0,0 +1,83 @@
+import { Subject } from "@prisma/client";
+import { Services } from ".";
+import { NotFoundError } from "../Errors/ClientError";
+
+
+class SubjectServices extends Services<"subject"> {
+    constructor() {
+        super("subject")
+    }
+
+    // Private Methods
+    private async findSubject(subjectId: number): Promise<Subject> {
+        const foundSubject = await this.findUnique({
+            where: { id: subjectId }
+        });
+        if(!foundSubject) {
+            throw new NotFoundError("Subject not found");
+        }
+
+        return foundSubject;
+    }
+
+    // Public Methods
+    public async createSubject(name: string, description: string, teacherId: number) {
+
+        const newSubject = await this.create({
+            data: {
+                name,
+                description,
+                teacherId,
+            }
+        });
+
+        return newSubject;
+    }
+
+    public async getSubjectById(subjectId: number) {
+
+        const foundSubject = await this.findUnique({
+            where: {id: subjectId},
+            include: {
+                teacher: true,
+                classes: true,
+            }
+        });
+
+        return foundSubject;
+    }
+
+    public async getAllSubject() {
+        
+        const subjects = await this.findMany({
+            include: {
+                teacher: true,
+                classes: true
+            }
+        });
+
+        return subjects;
+    }
+
+    public async updateSubject(subjectId: number, updateData: Partial<Omit<Subject, "id" | "createdAt" | "updatedAt">>) {
+
+        const foundSubject = await this.findSubject(subjectId);
+
+        const updatedSubject = await this.update({
+            where: {id: subjectId},
+            data: {
+                name: updateData.name || foundSubject.name,
+                description: updateData.description || foundSubject.description,
+                teacherId: updateData.teacherId || foundSubject.teacherId,
+            }
+        });
+
+        return updatedSubject;
+    }
+
+    public async deleteSubject(subject: Pick<Subject, "id">) {
+        await this.delete({
+            where: {id: subject.id}
+        })
+    }
+}
--- source/Services/TeacherServices.ts
+++ source/Services/TeacherServices.ts
@@ -2,17 +2,37 @@ import { inject, injectable } from "tsyringe";
 import { TOKENS } from "../Constants/tokensDI";
 import { ITeacherServices, IUserServices } from "../interfaces/Implements";
 import { TeacherUser } from "../Types/user";
-import { User } from "@prisma/client";
+import { Teacher, User } from "@prisma/client";
+import { Services } from ".";
+import { BadRequestError, NotFoundError } from "../Errors/ClientError";
 
 
 @injectable()
-class TeacherServices implements ITeacherServices {
+class TeacherServices extends Services<"teacher"> implements ITeacherServices {
 
 
-    constructor(@inject(TOKENS.UserServices) private userServices: IUserServices) {}
+    constructor(@inject(TOKENS.UserServices) private userServices: IUserServices) {
+        super("teacher")
+    }
     
+    // Private Methods
+    private async findTeacher(teacherId: number): Promise<Teacher | null> {
+        const foundTeacher = await this.findUnique({
+            where: { id: teacherId}
+        });
+        return foundTeacher;
+    }
+
     // Public methods
-    async createTeacher(teacher: Omit<TeacherUser, "role">): Promise<User> {
+    public async validateTeacher(teacherId: number): Promise<void> {
+        const teacher = await this.findTeacher(teacherId);
+        if(teacher) {
+            throw new BadRequestError("Teachs already exists");
+        }
+    }
+
+    public async createTeacher(teacher: Omit<TeacherUser, "role">): Promise<User> {
+
         const newTeacher = await this.userServices.createUser(
             {
                 name: teacher.name,
